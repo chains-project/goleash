ed5775cc50a576d022557fb15559bffc598e9f34
Aman Sharma <mannu.poski10@gmail.com>
2024-10-21 12:11:53 +0200

    test: add test for build

---
+++ track_syscalls/main_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+	"strings"
+	"testing"
+	"time"
+)
+
+func StartTracer(args []string, buffer *bytes.Buffer) (*exec.Cmd, error) {
+	tracer := exec.Command(args[0], args[1:]...)
+
+	tracer.Stdout = buffer
+	tracer.Stderr = buffer
+
+	// Run the command in a goroutine
+	go func() {
+		err := tracer.Run()
+		if err != nil {
+			fmt.Printf("Error running tracer: %v\n", buffer.String())
+		}
+	}()
+
+	return tracer, nil
+}
+
+func StopTracer(cmd *exec.Cmd) error {
+	if cmd.Process != nil {
+		err := cmd.Process.Kill()
+		if err != nil {
+			return fmt.Errorf("failed to stop process: %v", err)
+		}
+	}
+	return nil
+}
+
+func TestBuild(t *testing.T) {
+	var outputBuffer bytes.Buffer
+	tracer, err := StartTracer([]string{"./bpf_loader", "-binary", "../testpkgs/basiccgo/basiccgo", "-mode", "build", "-mod-manifest", "../testpkgs/basiccgo/go.mod"}, &outputBuffer)
+	if err != nil {
+		t.Errorf("Error starting process: %v", err)
+	}
+
+	time.Sleep(2 * time.Second)
+
+	exec.Command("../testpkgs/basiccgo/basiccgo").Run()
+	if err != nil {
+		t.Errorf("Error running basiccgo: %v", err)
+	}
+
+	StopTracer(tracer)
+
+	var expectedSubstring string = "Go caller package: main"
+	var actualOutput string = outputBuffer.String()
+
+	if !strings.Contains(actualOutput, expectedSubstring) {
+		t.Errorf("Expected output to contain %s, got %s", expectedSubstring, actualOutput)
+	}
+}
