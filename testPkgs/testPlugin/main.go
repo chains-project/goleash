4abdaba692fe7b8265f894bbf4330263b23b5b89
carminecesarano <c.cesarano@hotmail.it>
2025-11-12 12:04:56 +0100

    Delete testPkgs directory

--- testPkgs/testPlugin/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import pluginloader "github.com/carminecesarano/mal_dependency/plugin"
-
-func main() {
-	pluginloader.LoadAndInvokePlugin("./plugin/plugin.so", "PluginFunc")
-}
--- testPkgs/testReflection/go.mod
+++
@@ -1,5 +0,0 @@
-module testReflection
-
-go 1.24.0
-
-require github.com/carminecesarano/mal_dependency v0.1.25 // indirect
